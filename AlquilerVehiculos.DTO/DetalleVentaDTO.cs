76a7703a8a11635f7fbdc2ff4c598f047e4f1e41
DawnyCTI <20221014@itla.edu.do>
2024-09-11 15:18:40 -0400

    # Clases DTO

---
+++ AlquilerVehiculos.DTO/DetalleVentaDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerVehiculos.DTO
+{
+    public class DetalleVentaDTO
+    {
+        public int? IdVehiculo { get; set; }
+
+        public int? Cantidad { get; set; }
+
+        public decimal? Precio { get; set; }
+
+        public decimal? Total { get; set; }
+    }
+}
---
+++ AlquilerVehiculos.DTO/VehiculoDTO.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlquilerVehiculos.DTO
+{
+    public class VehiculoDTO
+    {
+        public int IdVehiculo { get; set; }
+
+        public string? Nombre { get; set; }
+
+        public int? IdCategoria { get; set; }
+
+        public int? DescripcionCategoria { get; set; }
+
+        public int? Stock { get; set; }
+
+        public string? PrecioAlquiler { get; set; }
+
+        public int? EsActivo { get; set; }
+    }
+}
